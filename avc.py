661c1780b1f606f1e7033f92b2a9163d8bb0e680
Sam Oehlert <soehlert@es.net>
2016-12-01 05:11:30 -0600

    working!

--- avc.py
+++ avc.py
@@ -3,14 +3,18 @@
 import sys
 import argparse
 import os.path
-import glob
 import yaml
+import re
 
+regex = re.compile(r"\{\{.*\}\}")
 var_dirs = [ "vars", "defaults" ]
-var_files = []
-ansible_vars = []
+conf_files = []
+conf_vars = []
+template_files = []
 template_vars = []
+task_files = []
 task_vars = []
+missing_vars = []
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--role", help="Specify role to check against", dest='role_dir')
@@ -21,30 +25,88 @@ args = parser.parse_args()
 role = args.role_dir
 groups = args.group_vars
 
-for group in groups:
-    var_files.append(group)
+if groups:
+    for group in groups:
+        conf_files.append(group)
 
-def roles_vars_files(role_dir):
-    """ Find all role variable files to check against"""
-    for dirpath, dirname, filenames in os.walk(role):
+def get_vars_files(role_dir):
+    """ Function to grab all the files we will need """
+    for dirpath, dirnames, filenames in os.walk(role):
         for filename in [f for f in filenames if f.endswith(".yaml") or f.endswith(".yml")]:
             for x in var_dirs:
                 if x in dirpath:
-                    var_files.append(os.path.join(dirpath, filename))
-    return var_files
+                    conf_files.append(os.path.join(dirpath, filename))
+            if "tasks" in dirpath:
+                task_files.append(os.path.join(dirpath, filename))
+        for filename in [f for f in filenames if f.endswith(".j2")]:
+            if "templates" in dirpath:
+                template_files.append(os.path.join(dirpath, filename))
 
-def get_vars(var_files):
+    return conf_files, task_files, template_files
+
+def get_task_vars(task_files):
+    """ Grab all variables from task files """
+    for f in task_files:
+        with open(f) as task:
+            for line in task:
+                match = re.search(regex, line)
+                if match and "item" not in match.group():
+                    if match.group() not in task_vars:
+                        v = match.group()[3:-3]
+                        task_vars.append(v)
+    return task_vars
+
+def get_template_vars(template_files):
+    """ Grab all variables from template files """
+    for f in template_files:
+        with open(f) as template:
+            for line in template:
+                match = re.search(regex, line)
+                if match and "item" not in match.group():
+                    if match.group() not in template_vars:
+                        v = match.group()[3:-3]
+                        template_vars.append(v)
+    return template_vars
+
+def get_conf_vars(conf_files):
     """ Check group variable files """
-    for fn in var_files:
+    for fn in conf_files:
         with open(fn) as stream:
             yaml_vars = yaml.load(stream)
-            for k,v in yaml_vars.items():
-                ansible_vars.append(k)
-    return ansible_vars
+            if yaml_vars:
+                for k,v in yaml_vars.items():
+                    conf_vars.append(k)
+    return conf_vars
+
+def compare_vars(conf_vars, t_vars):
+    """ Checks to see if you have any missing variables """
+    c_t = list(set(conf_vars) - set(t_vars))
+    for v in c_t:
+        if "ansible" not in v:
+            missing_vars.append(v)
+    t_c = list(set(t_vars) - set(conf_vars))
+    for v in t_c:
+        if "ansible" not in v:
+            missing_vars.append(v)
 
 if __name__ == '__main__':
-    file_list = roles_vars_files(role)
-    a_vars = get_vars(var_files)
-    print file_list
-    print a_vars
+    c_files, ta_files, te_files = get_vars_files(role)
+    conf_vars = get_conf_vars(c_files)
+    if not conf_vars:
+        print "There are no configuration variables to test against"
+        exit(1)
 
+    ta_vars = get_task_vars(ta_files)
+    te_vars = get_template_vars(te_files)
+
+    if te_vars:
+        compare_vars(conf_vars,te_vars)
+    if ta_vars:
+        compare_vars(conf_vars,ta_vars)
+
+    if missing_vars:
+        print "Variables missing matches:"
+        for v in missing_vars:
+            print v
+    else:
+        print "All your variables are accounted for"
